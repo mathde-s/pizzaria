57ee0e5e36daf1b15610ab30309f52290228640a
matheus <matheus.pereira@zup.com.br>
2025-01-22 20:03:41 -0300

    feat: add clienteResponseDTO

---
+++ src/main/java/com/zup/pizzaria/dtos/ClienteResponseDTO.java
@@ -0,0 +1,48 @@
+package com.zup.pizzaria.dtos;
+
+public class ClienteResponseDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public ClienteResponseDTO(long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
